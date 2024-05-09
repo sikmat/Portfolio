3edeeb2d9f5c56ba743b07db2a39372246e81a04
sikelela <sikelelamathole.sm@gmail.com>
2024-05-09 16:13:59 +0200

    update

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+// Functionality to scroll to the top
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+// Nav burger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+// Close burger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) => 
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
+);
